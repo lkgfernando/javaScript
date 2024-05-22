68406a9421d67539a5577fdd55dcb2ffb5208ae4
lkgfernando <lkgfernando@gmail.com>
2024-05-21 22:41:53 -0300

    #Operadores

---
+++ 02_OPERADORES/js/script.js
@@ -0,0 +1,24 @@
+// //1 - Numbers
+// console.log(typeof 2);
+// console.log(typeof 5.14);
+// console.log(typeof -127);
+
+// // 2 - Ops. aritméticas
+// console.log(2 + 4);
+// console.log(10 - 5);
+// console.log(35 * 20);
+// console.log(35 / 20);
+
+// console.log(25 + (20 * 2));
+
+// console.log(3.14 * (0.25 ** 2))
+
+
+// //5 Simbolo especiais
+// console.log("Teste quebra de linha \n simbolo /n")
+// console.log("Espaçamento por tab \t teste")
+
+console.log("Fernando " + "Rodrigues");
+
+console.log(`A som de 2 + 2 é ${2 + 2}`);
+
