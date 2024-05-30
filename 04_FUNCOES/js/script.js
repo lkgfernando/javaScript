263046933cada0d05d233b37cdbdda0943d7b2da
lkgfernando <lkgfernando@gmail.com>
2024-05-30 13:37:07 -0300

    #ESCOPO ANINHADO

--- 04_FUNCOES/js/script.js
+++ 04_FUNCOES/js/script.js
@@ -32,3 +32,44 @@ const result = sumValues(a, b);
 
 console.log(result);
 console.log(sumValues(d, c));
+console.log("Lemão Galego");
+
+//Função em escopo
+
+let y = 20;
+
+function testing() {
+  let y = 10;
+  return console.log(`Y dentro da function: ${y}`);
+}
+
+console.log(`Y Fora da function: ${y} `);
+
+testing();
+
+//Escopo aninhado
+console.log("*************** ESCOPO ANINHADO ********************");
+let x = 20;
+
+function nestedScope() {
+  let x = 30;
+
+  if (true) {
+    let x = 40;
+    console.log(`Primeiro IF: ${x}`);
+
+    if (true) {
+      let x = 50;
+      console.log(`Segundo IF: ${x}`);
+    }
+
+  }
+  console.log(`Escopo da função: ${x}`);
+}
+
+nestedScope();
+
+console.log(`Escopo global: ${x}`);
+
+
+
